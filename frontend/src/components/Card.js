3623edcb9c404d17fce22679de0729b43936da7e
Anjani kumar <anjanikumar70157@gmail.com>
2024-11-23 03:21:55 +0530

    just testing

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -9,6 +9,16 @@ function App() {
       imageUrl: " https://imgs.search.brave.com/qpWKmEBSVFV6azClAAZSqb9C2fNMWLbZaXhYJ_tlwFw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvc3RlcGhl/bi53ZWJw",
     },
     {
+      id: '853302',
+      experience: '25 year',
+      imageUrl: " https://imgs.search.brave.com/qpWKmEBSVFV6azClAAZSqb9C2fNMWLbZaXhYJ_tlwFw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvc3RlcGhl/bi53ZWJw",
+    },
+    {
+      id: '853302',
+      experience: '25 year',
+      imageUrl: " https://imgs.search.brave.com/qpWKmEBSVFV6azClAAZSqb9C2fNMWLbZaXhYJ_tlwFw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvc3RlcGhl/bi53ZWJw",
+    },
+    {
       id: '20019',
       experience: '10 year',
       imageUrl: 'https://imgs.search.brave.com/bQDraoG_AAPe1jsb6cljXafkoD8bat2uVnM7o2_OMsU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvYmlndHV0/b3IxLndlYnA',
