70aa3d3e0fd1a8cdfefe94b19be221a05c8f190c
Amankrsingh58 <amankrsingh58@gmail.com>
2024-11-23 18:42:27 +0530

    homepage updat

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -5,18 +5,11 @@ function Container(){
     return(
 <div className="container">
  <img src={pic}alt="picture"/>
- <hr></hr>
- <br></br>
- <div>
-
-
- <b className="text">Not sure? Take a free online counselling class from GharPeShiksha </b>
- <br></br>
- <b className="text2" >and clear your confusions.</b>
- <br></br>
- <br></br>
  
- <hr></hr>
+ <div className="demo">
+
+ <p className="text">Not sure? Take a free online counselling class from GharPeShiksha <br/>
+  and clear your confusions.</p>
  <button className="button">BOOK YOUR FREE DEMO</button>
  </div>
 </div>
