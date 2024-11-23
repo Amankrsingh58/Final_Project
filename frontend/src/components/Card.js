f0da904630d68bdf26498a965d155c5ecd45b960
Anjani kumar <anjanikumar70157@gmail.com>
2024-11-23 14:42:21 +0530

    slider done

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './Card.css';
+// import './Card.css';
 
 function App() {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 2,
     
-  };
+};
+
+
   const tutors = [
     {
       id: '853302',
@@ -28,7 +30,12 @@ function App() {
     {
       id: '20019',
       experience: '10 year',
-      imageUrl: 'https://imgs.search.brave.com/prXoJXDh9S3JUT8vsvIFFfOmClBCezdwoGVd79zp9Gk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvY2hyaXN0/aW5lLndlYnA',
+      imageUrl: 'https://imgs.search.brave.com/TmPjdkKcHXFNMf14Owwuwh7IZ3FMD8MKJjZXtylKIQM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvam9zaHVh/LndlYnA',
+    },
+    {
+      id: '20019',
+      experience: '10 year',
+      imageUrl: 'https://imgs.search.brave.com/nkNR7hH6AJM__x9b1B7N5kxdDYBSQjsBuWbweQuKwso/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMxLnd5emFudGNk/bi5jb20vaW1hZ2Vz/L3Nlby1zdG9jay10/dXRvcnMvYmlndHV0/b3IyLndlYnA',
     },
     {
       id: '20019',
@@ -38,10 +45,11 @@ function App() {
   ];
 
   return (
-    <div className="card">
-      <h2>Verified Tutor</h2>
+    <div className="card " >
+      <h1>our Top Tutors </h1>
+  
+      <div className='tutor-container' >
       <Slider {...settings}>
-      <div className="tutor-container">
         {tutors.map((tutor, index) => (
           <div className="tutor-card" key={index}>
             <img src={tutor.imageUrl} alt="Tutor" className="tutor-image" />
@@ -52,8 +60,10 @@ function App() {
             <button className="contact-btn">Contact This Tutor</button>
           </div>
         ))}
+         </Slider>    
       </div>
-      </Slider>
+    
+ 
     </div>
   );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Card from '../components/Card'
+import './Home.css'
 
 function Home() {
   return (
