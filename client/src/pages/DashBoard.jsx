0996ce26a09273f6b60122d7f00809779d4f7366
Amankrsingh58 <amankrsingh58@gmail.com>
2025-03-29 14:09:03 +0530

    again fix

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -22,7 +22,7 @@ const DashBoard = () => {
 // },[]);
 
   return (
-    <div className="flex h-screen mt-[64vh]">
+    <div className="flex h-screen mt-[64px]">
       <Sidebar />
       <div className="dashboard-content bg-[#2C2D33]">
 
