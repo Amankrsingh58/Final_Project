a32fe642f22d84828eb2c10c4f6a4496c4d1d3d2
Amankrsingh58 <amankrsingh58@gmail.com>
2025-03-29 18:38:45 +0530

    All Done

--- client/src/pages/StudentsDashboard.jsx
+++ client/src/pages/StudentsDashboard.jsx
@@ -21,7 +21,7 @@ const Navigate = useNavigate();
 // },[]);
 
   return (
-    <div className="flex pb-10 lg-pb-0  mt-[64px]  w-screen" style={{ height: 'calc(100vh - 64px)' }}>
+    <div className="flex pb-10 lg-pb-0  mt-[64px] fixed w-screen" style={{ height: 'calc(100vh - 64px)' }}>
       <Sidebar />
       <div className="bg-gray-100 h-auto dashboard-content flex-grow w-full overflow-x-hidden">
 
