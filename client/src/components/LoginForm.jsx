a605c6e00e190407fe5deb82f19269928ce15631
Amankrsingh58 <165487982+Amankrsingh58@users.noreply.github.com>
2025-08-15 01:55:30 +0530

    Update LoginForm.jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -153,7 +153,7 @@ const Login = () => {
                 id="remember-me"
                 name="remember-me"
                 type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                className="h-4 cursor-pointer w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
               />
               <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
                 Remember me
